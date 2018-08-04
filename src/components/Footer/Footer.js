f380c7bbb0f90a2ab6f0f6255a5c327930435894
Evgenii Khromkov <jk76@yandex-team.ru>
2018-08-04 13:04:19 +0300

    changes * start add css constants * refactor menu and footer for mobile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,9 @@ import './Footer.scss';
 const links = ['Помощь', 'Обратная связь', 'Разработчикам', 'Условия использования'];
 
 const Footer = ({ cls }) => `<div class="Footer ${cls}">
-  <div class="Footer__left">${links
-    .map(link => Framework.createElement(Link, { cls: 'Footer__link' }, link))
-    .join('')}</div>
+  <div class="Footer__left">
+    ${links.map(link => Framework.createElement(Link, { cls: 'Footer__link' }, link)).join('')}
+  </div>
   <div class="Footer__right">© 2001–2017  ООО «Яндекс»</div>
 </div>`;
 
