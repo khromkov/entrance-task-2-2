2c7d3d493498294f0d93afafb3c8fc16139b114d
Evgenii Khromkov <jk76@yandex-team.ru>
2018-08-12 14:02:22 +0300

    changes * add support hi resolution for logo and bg images

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.scss';
 const Header = ({ cls }) => `
   <div class="Header">
     <div class="Header__container ${cls}">
-      <img class="Header__logo" src="/img/logo@1x.png" alt="Яндекс Дом" />
+      <img class="Header__logo" srcset="/img/logo@1x.png, /img/logo@2x.png 2x" src="/img/logo@1x.png" alt="Яндекс Дом" />
       ${Framework.createElement(Menu, { cls: 'Header__menu' })}
     </div>
   </div>
